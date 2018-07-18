af6cfc5d7130763f0fd1638dd78ab3942c50f31d
Karl Clinckspoor <karlclinckspoor@protonmail.com>
2018-07-18 18:31:15 -0300

    Img png->pdf, visual DSC

--- imagens/Configurações das figuras.py
+++ imagens/Configurações das figuras.py
@@ -1,2 +1,8 @@
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+import matplotlib as mpl
 mpl.rcParams.update({'font.size': 14, 'text.usetex':False, 'text.latex.unicode':True})
 mpl.rcParams.update({'mathtext.fontset':'dejavusans'})
+mpl.rcParams['axes.formatter.use_locale'] = True
+
