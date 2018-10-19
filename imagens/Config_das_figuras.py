ad5f0740bd6a1e278cb14761f2d95dd722ba2513
Karl Clinckspoor <karlclinckspoor@protonmail.com>
2018-10-19 19:48:47 -0300

    TEO: cdots. Plots de reologia RH, ITC WLM e CMC

--- imagens/Config_das_figuras.py
+++ imagens/Config_das_figuras.py
@@ -2,7 +2,13 @@ import locale
 locale.setlocale(locale.LC_ALL, '')
 
 import matplotlib as mpl
-mpl.rcParams.update({'font.size': 14, 'text.usetex':False, 'text.latex.unicode':True})
+mpl.rcParams.update({'font.size': 14, 'text.usetex':False})
 mpl.rcParams.update({'mathtext.fontset':'dejavusans'})
 mpl.rcParams['axes.formatter.use_locale'] = True
 
+dict_simbolos = {'Água':'o',
+                 'Sacarose':'s',
+                 'Glicerina':'<',
+                 'DMSO':'>',
+                 '1,3BD':'^',
+                 'Ureia':'v'}
--- python/plot_saxs_esfera.py
+++ python/plot_saxs_esfera.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.special as sp
 
 def I_esfera (q, R):
     qR = q * R
